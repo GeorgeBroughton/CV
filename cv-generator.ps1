836fb198d52642a4769332b9c8e4ee0ea28a8367
GeorgeBroughton <george.daniel.broughton@gmail.com>
2023-03-13 14:47:04 +0000

    Added some icons, updated some of the formatting

--- cv-generator.ps1
+++
@@ -1,4 +0,0 @@
-# The purpose of this file is to build both the MD and LaTeX code for my CV without me having to update it manually.
-# Headings and sections like work experience will be generated from JSON objects imported from elsewhere.
-# No plans to do anything here just yet, because i'm figuring out the formatting, but this will be updated once I
-# have received enough feedback about my CV.
